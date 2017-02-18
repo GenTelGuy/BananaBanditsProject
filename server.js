672e7d8b6cc5a211fde662790fb17e0d8614b9d4
rluskDev <rlusk@ucsd.edu>
2017-02-18 13:41:17 -0800

    changed db

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const MongoClient = require('mongodb').MongoClient;
 
 var path = require('path');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'))
 app.use('/images', express.static(__dirname + 'public'));
 //app.use(express.static(path.join(__dirname, 'public')));
@@ -17,13 +17,13 @@ app.use('/images', express.static(__dirname + 'public'));
 var db
 //var view = 'index.ejs'
 
-MongoClient.connect('mongodb://cse110:banana@ds137139.mlab.com:37139/cse110', (err, database) => {
-        if(err) return console.log(err);
-        db = database;
-        app.listen(3000, () => {
-            console.log('listening on 3000');
-        });
-        });
+MongoClient.connect('mongodb://me:123qwe@ds153609.mlab.com:53609/practice-database', (err, database) => {
+  if (err) return console.log(err);
+  db = database;
+  app.listen(3000, () => {
+    console.log('listening on 3000');
+  });
+});
 
 /*app.listen(3000, function(){
   console.log('listening on 3000');
@@ -32,13 +32,13 @@ MongoClient.connect('mongodb://cse110:banana@ds137139.mlab.com:37139/cse110', (e
 
 
 app.post('/quotes', (req, res) => {
-    console.log(req.body)
-    db.collection('quotes').save(req.body, (err, result) => {
-        if (err) return console.log(err)
+  console.log(req.body)
+  db.collection('quotes').save(req.body, (err, result) => {
+    if (err) return console.log(err)
 
-        console.log('saved to database')
-        res.redirect('/')
-    })
+    console.log('saved to database')
+    res.redirect('/')
+  })
 
 })
 
@@ -49,14 +49,14 @@ app.get('/', (req, res) => {
     //res.render('index.ejs', {quotes: result})
     console.log(result);
   })*/
-    res.sendFile(__dirname + '/index.html')
+  res.sendFile(__dirname + '/index.html')
 })
 
-app.get('/data', function(req, res){
+app.get('/data', function (req, res) {
   //res.send('hello world'); //replace with your data here
-  db.collection('quotes').find().toArray(function(err, results){
-        console.log(results);
-        res.send(results);
+  db.collection('quotes').find().toArray(function (err, results) {
+    console.log(results);
+    res.send(results);
   });
 });
 
