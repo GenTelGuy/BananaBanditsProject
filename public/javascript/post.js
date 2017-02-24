87bf3c9167005b5d515a46ac93ac18e6bbb92665
rluskDev <rlusk@ucsd.edu>
2017-02-23 19:50:11 -0800

    Fixed search function, but date unformatted

--- public/javascript/post.js
+++ public/javascript/post.js
@@ -1,9 +1,8 @@
-
-$( document ).ready(function() {
-    $('#updateEvents').click(function(){
+$(document).ready(function () {
+    $('#updateEvents').click(function () {
         updateEventsList();
     });
-    $('#searchBar').keyup(function(){
+    $('#searchBar').keyup(function () {
         filterEvents();
         updateEventDivs();
     });
@@ -12,16 +11,16 @@ $( document ).ready(function() {
 
 });
 
-function updateEventsList(){
-    response = $.get('http://localhost:3000/data', {}, function(data){
+function updateEventsList() {
+    response = $.get('http://localhost:3000/data', {}, function (data) {
         $('#eventList').empty();
         allEvents = [];
-        for(i=0; i<data.length; i++){
+        for (i = 0; i < data.length; i++) {
             createdDiv = eventDiv(data[i]);
             a = {
                 visible: true,
-                searchableText: [data[i].Title.toLowerCase(), 
-                                data[i].Description.toLowerCase()],
+                searchableText: [data[i].Title.toLowerCase(),
+                data[i].Details.toLowerCase()],
                 div: createdDiv
             }
             allEvents.push(a);
@@ -30,56 +29,70 @@ function updateEventsList(){
     });
 }
 
-function filterEvents(){
+function filterEvents() {
     searchString = $('#searchBar').val();
-    for(i=0; i<allEvents.length; i++){
+    for (i = 0; i < allEvents.length; i++) {
         evt = allEvents[i];
-        match=false;
-        for(j=0; j<evt.searchableText.length; j++){
-            str=evt.searchableText[j];
-            if(str.includes(searchString.toLowerCase()) || 
-                str===""){
-                match=true;
+        match = false;
+        for (j = 0; j < evt.searchableText.length; j++) {
+            str = evt.searchableText[j];
+            if (str.includes(searchString.toLowerCase()) ||
+                str === "") {
+                match = true;
             }
         }
-        evt.visible=match;
+        evt.visible = match;
     }
-        
+
 }
 
-function updateEventDivs(){
-    for (i=0; i<allEvents.length; i++){
-        evt=allEvents[i];
-        if(evt.visible){
-            evt.div.style.display='block';
+function updateEventDivs() {
+    for (i = 0; i < allEvents.length; i++) {
+        evt = allEvents[i];
+        if (evt.visible) {
+            evt.div.style.display = 'block';
         }
-        else{
-            evt.div.style.display='none';
+        else {
+            evt.div.style.display = 'none';
         }
     }
 }
 
 
-function eventDiv(data){
+function eventDiv(data) {
 
-    ret = document.createElement( "div" );
-    ret.className="Event";
+    ret = document.createElement("div");
+    ret.className = "Event";
 
-    header=document.createElement("h3");
+    header = document.createElement("h3");
     header.appendChild(document.createTextNode(data.Title));
     ret.appendChild(header);
 
-    startTime=document.createElement("p");
-    startTime.appendChild(document.createTextNode("Start time: " + data.StartMonth + "/" + data.StartDay));
+    startTime = document.createElement("p");
+    //var date = correctDate(data.StartTime);
+    startTime.appendChild(document.createTextNode("Start time: " + data.StartTime));
     ret.appendChild(startTime);
 
-    descriptionHeader=document.createElement("h1");
-    descriptionHeader.appendChild(document.createTextNode("Description: "));
+    descriptionHeader = document.createElement("h1");
+    descriptionHeader.appendChild(document.createTextNode("Details: "));
     ret.appendChild(startTime);
 
-    descriptionText=document.createElement("p");
-    descriptionText.appendChild(document.createTextNode(data.Description));
+    descriptionText = document.createElement("p");
+    descriptionText.appendChild(document.createTextNode(data.Details));
     ret.appendChild(descriptionText);
-    
-    return(ret);
+
+    return (ret);
 }
+
+/*function correctDate(date) {
+    var dateString = "Start Time: " + date.getMonth() + "/" + date.getDay() + "/" + date.getFullYear() + " ";
+    console.log(dateString);
+    var hour = date.getHours();
+    var minutes = date.getMinutes();
+    if (hour > 12)
+        return dateString + (hour - 12) + ":" + minutes + "PM";
+    if (hour == 12)
+        return dateString + hour + ":" + minutes + "PM";
+    return dateString + hour + ":" + minutes + "AM";
+    } */
+
