864ed944dafb36eb60e0480f31ca7264998e8f93
rluskDev <rlusk@ucsd.edu>
2017-03-05 13:39:34 -0800

    Accounts now synced with database

--- mongoApp/routes/index.js
+++ mongoApp/routes/index.js
@@ -1,68 +1,74 @@
 var express = require('express');
 var router = express.Router();
-var db = require('monk')('localhost:27017/test');
-var userData = db.get('userData');
+var db = require('monk')('mongodb://cse110:banana@ds137139.mlab.com:37139/cse110');
+var userData = db.get('accounts');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+	res.render('index', { title: 'Express' });
 });
 
-router.get('/login/:email', function(req, res, next) {
+router.get('/login/:email', function (req, res, next) {
 	var retEmail = req.params.email;
-    res.render('loggedin', {email: retEmail});
-    console.log('retEmail');
+	res.render('loggedin', { email: retEmail });
+	console.log('retEmail');
 });
 
-router.get('/createAccount', function(req, res, next){
+router.get('/createAccount', function (req, res, next) {
 	res.render('createAccount');
 });
 
-router.post('/createAccount/submit', function(req, res, next){
-	var organizationName = req.body.orgName;
-	var organizationEmail = req.body.orgEmail;
-	var organizationPassword = req.body.orgPassword;
+router.post('/createAccount/submit', function (req, res, next) {
 
-	var item = {
-		name: organizationName,
-		email:organizationEmail,
-		password:organizationPassword
+	var account = {
+		Org: req.body.orgName,
+		Email: req.body.orgEmail,
+		Password: req.body.orgPassword,
+		OrgBio: "",
+		Verified: false,
+		Admin: false
 	};
 
-	userData.insert(item);
-	res.render('accSubmit', {name: organizationName});
+	userData.insert(account);
+	res.render('accSubmit', { name: req.body.orgName });
 });
 
-router.post('/login/submit', function(req, res, next){
+router.post('/login/submit', function (req, res, next) {
 	var orgEmail = req.body.email;
 	var orgPassword = req.body.password;
 
-	var data = userData.findOne({$and:[{"email": orgEmail}, {"password": orgPassword}]});
-	data.then(function(data){
+	var data = userData.findOne({ $and: [{ "Email": orgEmail }, { "Password": orgPassword }] });
+	data.then(function (data) {
 		console.log(data);
-		if(data == null) {
+		if (data == null) {
 			res.redirect('/notExist');
 		}
-    	res.redirect('/login/' + orgEmail);
-    	
-    });	
-});
-
-router.get('/notExist', function(req, res, next){
-	res.render('notExist');
-});
-
-router.get('/listAccounts', function(req, res, next){
-	var list = userData.find({});
-	list.then(function(data){
-		console.log(data);
-		res.render('listAccounts', {listOf: data, helpers: { json: function (context) { 
-			return JSON.stringify(context); } }});
+		else{
+		  res.redirect('/login/' + orgEmail);
+		}
 	});
 });
 
-router.get('/deleteAll', function(req, res, next){
-	userData.remove({}).then(function(data){
+router.get('/notExist', function (req, res, next) {
+	res.render('notExist');
+});
+
+router.get('/listAccounts', function (req, res, next) {
+	var list = userData.find({});
+	list.then(function (data) {
+		console.log(data);
+		res.render('listAccounts', {
+			listOf: data, helpers: {
+				json: function (context) {
+					return JSON.stringify(context);
+				}
+			}
+		});
+	});
+});
+
+router.get('/deleteAll', function (req, res, next) {
+	userData.remove({}).then(function (data) {
 		res.redirect('/listAccounts');
 	});
 });
