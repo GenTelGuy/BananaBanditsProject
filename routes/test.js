cfafc9df5e19d6c33caa475e683155c4cf1580a2
rluskDev <rlusk@ucsd.edu>
2017-03-05 13:12:24 -0800

    deleted useless stuff

--- routes/test.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/dog', function (req, res, next) {
-	res.render('test', {title: 'Express'});
-});
-
-module.exports = router;
