51030d72510952ce1d25b027d46bfb17b21731ae
Shaheryar Ajmal <sherio777@gmail.com>
2017-03-11 15:34:03 -0800

    A lot of the edit work is done

--- mongoApp/app.js
+++ mongoApp/app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var hbs = require('express-handlebars');
 var session = require('express-session');
+//var bodyParser = require('body-parser');
 //var session = require('client-sessions');
 
 var index = require('./routes/index');
@@ -26,7 +27,7 @@ app.set('view engine', 'hbs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
--- mongoApp/routes/index.js
+++ mongoApp/routes/index.js
@@ -29,17 +29,36 @@ router.get('/login/:email', function (req, res, next) {
 
 router.get('/createAccount', function (req, res, next) {
 	console.log(req.session);
-	res.render('createAccount');
+	if(!req.session){
+		res.render('notAdmin');
+	}
+	//else {
+	//	if(!req.session.user.Admin) {
+	//		res.render('notAdmin');
+	//	}
+		else {
+			res.render('createAccount');
+		}
+	//}
+
 });
 
 router.post('/createAccount/submit', function (req, res, next) {
+	var admin = req.body.admin;
+	var json;
+	if(admin === "true") {
+		json = true;
+	}
+	else {
+		json = false;
+	}
 	var account = {
 		Org: req.body.orgName,
 		Email: req.body.orgEmail,
 		Password: req.body.orgPassword,
 		OrgBio: "",
 		Verified: false,
-		Admin: false
+		Admin: json
 	};
 
 	userData.insert(account);
@@ -47,6 +66,11 @@ router.post('/createAccount/submit', function (req, res, next) {
 });
 
 router.post('/login/submit', function (req, res, next) {
+	console.log(req.session);
+	if(!req.session) { 
+		console.log("wtf");
+		req.session.destroy(); 
+	}
 	var orgEmail = req.body.email;
 	var orgPassword = req.body.password;
 
@@ -74,10 +98,10 @@ router.post('/login/submit', function (req, res, next) {
 	});*/
 });
 
-router.post('/dashbaord', function(req, res, next){
+router.get('/dashboard', function(req, res, next){
 	console.log(req.session);
 	//res.render('listAccounts.hbs');
-	if(!req.session.user) {
+	if(!req.session.user.Admin) {
 		res.redirect('/listAccounts');
 
 		//return res.status(401);
@@ -88,6 +112,27 @@ router.post('/dashbaord', function(req, res, next){
 	}
 });
 
+router.get('/forgetPassword', function(req, res) {
+	res.render('forgetPassword');
+});
+
+router.post('/forgetPassword/submit', function(req, res){
+	var email = req.body.email;
+	var password = req.body.password;
+	var confirmPass = req.body.confirmPassword;
+
+	if(password != confirmPass) {
+		res.render('notExist', {message: "passwords do not match"});
+	}
+	userData.update({"Email": email}, {$set: {"Password": password}}, function(err, user){
+		console.log(user);
+		if(!user){
+			res.redirect('/notExist');
+		}
+
+		res.redirect('/');
+	});
+});
 router.get('/logout', function(req, res){
 	req.session.destroy();
 	res.render('index');
@@ -112,6 +157,30 @@ router.get('/listAccounts', function (req, res, next) {
 	//req.session.reset();
 });
 
+router.get('/verifyAccounts', function(req, res){
+	if(req.session.user.Admin == false) {
+		res.render('notAdmin');
+	}
+	else {
+		userData.find({"Verified": false}, function(err, user){
+			//console.log(user);
+			res.render('listUnverified', {listOf: user});
+		});
+	}
+});
+
+router.post('/verify', function(req, res) {
+	
+		var name = req.body.orgName;
+		console.log(name);
+		userData.update({"Org": name}, {$set: {"Verified": true}}, function(err, user) {
+			console.log(user);
+			res.redirect('/verifyAccounts');
+		});
+
+		//res.redirect('/');
+});
+
 function requireLogin (req, res, next) {
   if (!req.user) {
     res.redirect('/');
@@ -120,9 +189,81 @@ function requireLogin (req, res, next) {
   }
 };
 
-router.get('/deleteAll', requireLogin, function (req, res) {
+router.get('/deleteAll', function (req, res) {
 	userData.remove({}).then(function (data) {
 		res.redirect('/listAccounts');
+	});
+});
+
+router.post('/deleteEvent', function(req, res){
+	var name = req.body.eventname;
+	console.log(name);
+	console.log('9999999');
+	eventData.remove({"Title": name}, function(err, user) {
+		//console.log(user);
+		res.redirect('/eventBrowser');
+	});
+});
+
+router.get('/checkAdmin', function(req, res, next){
+	if(!req.session.user.Admin) {
+		console.log("not admin");
+		res.send('not admin');
+	}
+	else { 
+		console.log("admin");
+		res.send('admin');
+	}
+});
+
+router.post('/pinEvent', function(req, res) {
+	var eventname = req.body.eventpin;
+	eventData.update({"Title": eventname}, {$set: {"Featured": true}}, function (err, user){
+		res.redirect('/eventBrowser');
+	});
+});
+
+router.post('/edit', function(req, res){
+	console.log("We're chillin at edit. Post worked.")
+	var id = req.body.editevent;
+	/**var timeStart;
+	var timeEnd;
+	var description;
+	var id;*/
+
+	/**eventData.findOne({"Title": title}, function(err, user) {
+		console.log(user);
+		timeStart = user.StartTime;
+		timeEnd = user.EndTime;
+		description = user.Details;
+		id = user._id;*/
+
+		res.redirect('/editEvent/' + id);
+		//res.redirect('/editEvent/' + title + '/' + timeStart + '/' + timeEnd + '/' + description + '/' + id);
+	//});
+});
+
+router.get('/editEvent/:id', function(req, res) {
+	console.log("We are at edit event. The get function is working");
+	var identification = req.params.id;
+	eventData.findOne({"_id": identification}, function(err, evt) {
+		res.render('editEvent', {name: evt.Title, start: evt.StartTime, end: evt.EndTime, description: evt.Details, id: identification});
+	})
+})
+/**router.get('/editEvent/:title/:timeStart/:endTime/:details/:id', function(req, res) {
+	console.log("We are at edit event. The get function is working");
+	res.render('editEvent', { name: req.params.title, start: req.params.timeStart, end: req.params.endTime, description: req.params.details, id: req.params.id});
+});*/
+
+router.post('/updateEvent', function(req, res) {
+	var id = req.body.eventID;
+	var oldTitle = req.body.originalTitle;
+	var title = req.body.title;
+	var details = req.body.Description;
+	var startTime = req.body.StartYear + "-" + req.body.StartMonth + "-" + req.body.StartDay + "T" + req.body.StartHour + ":" + req.body.StartMinute + ":00-08:00Z";
+	var endTime = req.body.EndYear + "-" + req.body.EndMonth + "-" + req.body.EndDay + "T" + req.body.EndHour + ":" + req.body.EndMinute + ":00-08:00Z";
+	eventData.update({"Title": oldTitle}, {$set: {"Title": title, "Details": details, "StartTime": startTime, "EndTime": endTime}}, function(err, user) {
+		res.redirect('http://localhost:3000/eventDetail?query=' + id);
 	});
 });
 
@@ -176,6 +317,7 @@ router.get('/allEventData', function (req, res, next) {
 
 router.get('/eventDetail', function (req, res, next) {
 	res.render('EventDetail')
+
 });
 
 router.get('/eventDetailQuery', function (req, res, next) {
